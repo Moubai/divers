cfd3698eaf5f8bf62af0f4e36c485de3e74abc09
Moubai <1314184+Moubai@users.noreply.github.com>
2024-07-29 13:15:47 +0200

    Update Infomaniak_updateDDNS.ps1

--- Infomaniak_updateDDNS.ps1
+++ Infomaniak_updateDDNS.ps1
@@ -10,6 +10,7 @@ $username = "myusername"
 $password = "mysuperpassword"
 
 #api url from infomaniak source doc :https://www.infomaniak.com/fr/support/faq/2376/utiliser-dyndns-par-api-infomaniak
+#if myip is not defined it use the external ip of the client by default (see doc from infomaniak)
 $apiurl = "https://infomaniak.com/nic/update?hostname=$hostname&myip=$ip&username=$username&password=$password"
 $response = Invoke-restMethod -uri $apiurl -method Post
 $response

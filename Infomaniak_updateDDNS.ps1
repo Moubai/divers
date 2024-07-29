b0f596d8da0754e1bc92f8017e6d50b138470545
Moubai <1314184+Moubai@users.noreply.github.com>
2024-07-29 13:14:46 +0200

    Create Infomaniak_updateDDNS.ps1

---
+++ Infomaniak_updateDDNS.ps1
@@ -0,0 +1,16 @@
+### script to update infomaniak.com DDNS with powershell
+# if you need transcript for Schedule Task
+#Start-Transcript -path "c:\ddns\logs\transcript.txt" -append
+$hostname = "subdomain.mydomain.tld"
+
+# you can choose another website like apify but need to adapt the powershell object
+#ifconfig return only the ip this is why i choose it.
+$ip = curl ifconfig.me
+$username = "myusername"
+$password = "mysuperpassword"
+
+#api url from infomaniak source doc :https://www.infomaniak.com/fr/support/faq/2376/utiliser-dyndns-par-api-infomaniak
+$apiurl = "https://infomaniak.com/nic/update?hostname=$hostname&myip=$ip&username=$username&password=$password"
+$response = Invoke-restMethod -uri $apiurl -method Post
+$response
+#Stop-Transcript
